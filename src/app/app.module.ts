d991952cef6f7957bb0009614bc300bb8ca4eb9a
Saulo Moura <saulo.moura@sp01.local>
2019-05-10 17:15:09 -0300

    Estruturação da aplicação, consumo dos dados e resolução de problema com cross domain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { EventsService } from './services/events.service';
+
+import { MatToolbarModule, MatIconModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTreeModule,
+    MatIconModule,
+    MatToolbarModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EventsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
